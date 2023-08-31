ca7c049bc253be736596080b6adf74a73acfdf76
rafittu <rafittu@outlook.com>
2023-08-31 15:03:42 -0300

    feat: update property service

--- src/modules/property/services/update-property.service.ts
+++ src/modules/property/services/update-property.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PropertyRepository } from '../repository/property.repository';
+import { IPropertyRepository } from '../interfaces/repository.interface';
+import { UpdatePropertyDto } from '../dto/update-property.dto';
 
 @Injectable()
 export class UpdatePropertyService {
-  async execute() {
-    return 'property updated';
+  constructor(
+    @Inject(PropertyRepository)
+    private propertyRepository: IPropertyRepository,
+  ) {}
+
+  async execute(id: string, updatePropertyDto: UpdatePropertyDto) {
+    return await this.propertyRepository.updateProperty(id, updatePropertyDto);
   }
 }
