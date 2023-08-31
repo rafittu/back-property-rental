38dbd911e4ed43179ceb9368cca8e398f9be9324
rafittu <rafittu@outlook.com>
2023-08-31 09:17:51 -0300

    feat: create property rental service

--- src/modules/property/services/create-property.service.ts
+++ src/modules/property/services/create-property.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { IPropertyRepository } from '../interfaces/repository.interface';
+import { PropertyRepository } from '../repository/property.repository';
+import { CreatePropertyDto } from '../dto/create-property.dto';
 
 @Injectable()
 export class CreatePropertyService {
-  async execute() {
-    return 'property rental created';
+  constructor(
+    @Inject(PropertyRepository)
+    private propertyRepository: IPropertyRepository,
+  ) {}
+
+  async execute(createPropertyDto: CreatePropertyDto) {
+    return await this.propertyRepository.createPropertyRental(
+      createPropertyDto,
+    );
   }
 }
