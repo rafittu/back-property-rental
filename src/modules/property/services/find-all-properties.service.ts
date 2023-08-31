4e23608a432e0b858847155976049a5b0437ed13
rafittu <rafittu@outlook.com>
2023-08-31 11:34:39 -0300

    feat: find all properties service

--- src/modules/property/services/find-all-properties.service.ts
+++ src/modules/property/services/find-all-properties.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PropertyRepository } from '../repository/property.repository';
+import { IPropertyRepository } from '../interfaces/repository.interface';
 
 @Injectable()
 export class FindAllPropertiesService {
+  constructor(
+    @Inject(PropertyRepository)
+    private propertyRepository: IPropertyRepository,
+  ) {}
+
   async execute() {
-    return 'all properties for rental';
+    return await this.propertyRepository.findAllProperties();
   }
 }
