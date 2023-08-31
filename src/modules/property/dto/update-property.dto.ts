4247081784144dd47cc613db6c873207c5c1ac92
rafittu <rafittu@outlook.com>
2023-08-31 15:01:10 -0300

    label: update property dto

--- src/modules/property/dto/update-property.dto.ts
+++ src/modules/property/dto/update-property.dto.ts
@@ -1,4 +1,72 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePropertyDto } from './create-property.dto';
+import {
+  IsString,
+  IsNumber,
+  IsBoolean,
+  IsEnum,
+  IsOptional,
+} from 'class-validator';
+import { PropertyType } from '@prisma/client';
 
-export class UpdatePropertyDto extends PartialType(CreatePropertyDto) {}
+export class UpdatePropertyDto extends PartialType(CreatePropertyDto) {
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @IsOptional()
+  @IsString()
+  state?: string;
+
+  @IsOptional()
+  @IsString()
+  zipCode?: string;
+
+  @IsOptional()
+  @IsString()
+  bedrooms?: string;
+
+  @IsOptional()
+  @IsString()
+  bathrooms?: string;
+
+  @IsOptional()
+  @IsString()
+  garageSpaces?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  swimmingPool?: boolean;
+
+  @IsOptional()
+  @IsString()
+  size?: string;
+
+  @IsOptional()
+  @IsEnum(PropertyType)
+  type?: PropertyType;
+
+  @IsOptional()
+  @IsNumber()
+  rentalAmount?: number;
+
+  @IsOptional()
+  @IsNumber()
+  condoFee?: number;
+
+  @IsOptional()
+  @IsNumber()
+  propertyTax?: number;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
